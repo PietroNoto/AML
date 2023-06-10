8ef5c9f9a24ba0d8b9e27eb342ce7290c5cf41c2
francesco <s304910@studenti.polito.it>
2023-06-10 23:44:24 +0200

    adding plot function

--- main.py
+++ main.py
@@ -8,14 +8,57 @@ from stable_baselines3.common.monitor import Monitor
 from os.path import exists
 
 
+#plotting
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common import results_plotter
+import os
+import matplotlib.pyplot as plt
+def moving_average(values, window):
+    """
+    Smooth values by doing a moving average
+    :param values: (numpy array)
+    :param window: (int)
+    :return: (numpy array)
+    """
+    weights = np.repeat(1.0, window) / window
+    return np.convolve(values, weights, 'valid')
+
+def plot_results(log_folder, title='Learning Curve',filename="train_plot"):
+    """
+    plot the results
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), 'timesteps')
+    
+    #y = moving_average(y, window=50)
+    
+    # Truncate x
+    x = x[len(x) - len(y):]
+
+    fig,ax = plt.subplots()  #title
+    ax.plot(x, y)
+    ax.set_xlabel('Number of Timesteps')
+    ax.set_ylabel('Rewards')
+    ax.set_title(title + " Smoothed")
+    #plt.show()
+    fig.savefig(filename+".png")
+
+
+
+
 class Model:
 
-    def __init__(self, train_env_name: str, test_env_name: str):
+    def __init__(self, train_env_name: str, test_env_name: str,log_folder:str):
         
+        self.log_folder = log_folder
+        os.makedirs(self.log_dir, exist_ok=True)
+
         self.train_env_name = train_env_name
         self.test_env_name = test_env_name
-        self.train_env = Monitor(gym.make(train_env_name))
-        self.test_env = Monitor(gym.make(test_env_name))
+        self.train_env = Monitor(gym.make(train_env_name),self.log_folder)
+        self.test_env = Monitor(gym.make(test_env_name),self.log_folder)
         self.arch_name = None
 
 
@@ -70,6 +113,9 @@ class Model:
             self.mean_reward, self.std_reward = evaluate_policy(self.model, self.test_env, n_eval_episodes = n_eval, deterministic = True)
             print(f"mean_reward={self.mean_reward:.2f} +/- {self.std_reward:.2f}") 
 
+    def plot_results(self):
+        plot_results(log_folder=self.log_folder,filename=self.arch_name)
+
 if __name__ == '__main__':
 
     n_test_eps = 50
@@ -81,22 +127,28 @@ if __name__ == '__main__':
     #dummy.train_dummy()
     
     #Source-source
-    ss_model = Model("CustomHopper-source-v0", "CustomHopper-source-v0")
+    print("Source-source:")
+    s_log_dir="log_s_"+str(lr)+"_"+str(n_timesteps)
+    ss_model = Model("CustomHopper-source-v0", "CustomHopper-source-v0",s_log_dir)
     ss_model.train(None, n_timesteps, learning_rate = lr)
     ss_model.test(n_test_eps)
+    ss_model.plot_results()
     
     #Source-target
     st_model = Model("CustomHopper-source-v0", "CustomHopper-target-v0")
     st_model.train("SAC_s_0.003_50", n_timesteps, learning_rate = lr)
     st_model.test(n_test_eps)
+    st_model.plot_results()
 
     #Target-target
     tt_model = Model("CustomHopper-target-v0", "CustomHopper-target-v0")
     #tt_model.train(n_timesteps, learning_rate = lr)
     #tt_model.test(n_test_eps)
+    #tt_model.plot_results()
 
     #Source-source using UDR
-    ss_udr = Model("CustomHopper-source-v0", "CustomHopper-source-v0")
+    t_log_dir="log_t_"+str(lr)+"_"+str(n_timesteps)
+    ss_udr = Model("CustomHopper-source-v0", "CustomHopper-source-v0",t_log_dir)
     ss_udr.train_udr(None, n_timesteps, n_distr, learning_rate = lr)
     ss_udr.test(n_test_eps)
 
