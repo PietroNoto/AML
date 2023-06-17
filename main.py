2af92edce5790a1c1edec5e31bdaa6f3a87e72de
PietroNoto <s301332@studenti.polito.it>
2023-06-17 15:01:33 +0200

    Minor bug fix with plot function

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from stable_baselines3.common.results_plotter import load_results, ts2xy
 from stable_baselines3.common import results_plotter
 import os
 import matplotlib.pyplot as plt
+
+
 def moving_average(values, window):
     """
     Smooth values by doing a moving average
@@ -22,6 +24,7 @@ def moving_average(values, window):
     """
     weights = np.repeat(1.0, window) / window
     return np.convolve(values, weights, 'valid')
+
 
 def plot_results(log_folder, title='Learning Curve',filename="train_plot"):
     """
@@ -35,7 +38,7 @@ def plot_results(log_folder, title='Learning Curve',filename="train_plot"):
     #y = moving_average(y, window=50)
     
     # Truncate x
-    x = x[len(x) - len(y):]
+    #x = x[len(x) - len(y):]
 
     fig,ax = plt.subplots()  #title
     ax.plot(x, y)
@@ -53,7 +56,7 @@ class Model:
     def __init__(self, train_env_name: str, test_env_name: str,log_folder:str):
         
         self.log_folder = log_folder
-        os.makedirs(self.log_dir, exist_ok=True)
+        os.makedirs(self.log_folder, exist_ok=True)
 
         self.train_env_name = train_env_name
         self.test_env_name = test_env_name
@@ -72,17 +75,12 @@ class Model:
                 arch_name += "t_"
             arch_name = arch_name + str(hyperparams["learning_rate"]) + '_' + str(timesteps)
             self.model = SAC(MlpPolicy, self.train_env, verbose = 1, **hyperparams)
-            self.model.learn(total_timesteps = timesteps, log_interval = 20)
+            self.train_env.reset()
+            self.model.learn(total_timesteps = timesteps, log_interval = 10)
             self.model.save(arch_name)   
         elif exists(arch_name):
             self.model = SAC.load(arch_name)
         self.arch_name = arch_name
-
-
-    def train_dummy(self):
-
-        self.model = SAC(CnnPolicy, self.train_env, verbose = 1)
-        self.model.learn(500)
 
 
     def train_udr(self, arch_name = None, timesteps = 50000, n_distr = 3, **hyperparams):
@@ -113,48 +111,58 @@ class Model:
             self.mean_reward, self.std_reward = evaluate_policy(self.model, self.test_env, n_eval_episodes = n_eval, deterministic = True)
             print(f"mean_reward={self.mean_reward:.2f} +/- {self.std_reward:.2f}") 
 
+
     def plot_results(self):
-        plot_results(log_folder=self.log_folder,filename=self.arch_name)
+        e = "s" if self.test_env_name == "CustomHopper-source-v0" else "t"
+        filename = self.arch_name[:5] + e + self.arch_name[5:]
+        plot_results(log_folder=self.log_folder,filename = filename)
+
+
 
 if __name__ == '__main__':
 
     n_test_eps = 50
-    n_timesteps = 50
-    lr = 0.003
+    n_timesteps = 50000
+    lr = 0.03
     n_distr = 3
 
-    #dummy = Model("CustomHopper-source-v0", "CustomHopper-source-v0")
-    #dummy.train_dummy()
     
     #Source-source
     print("Source-source:")
-    s_log_dir="log_s_"+str(lr)+"_"+str(n_timesteps)
+    s_log_dir = "log_ss_" + str(lr) + "_" + str(n_timesteps)
     ss_model = Model("CustomHopper-source-v0", "CustomHopper-source-v0",s_log_dir)
-    ss_model.train(None, n_timesteps, learning_rate = lr)
+    ss_model.train("SAC_s_0.03_50000", n_timesteps, learning_rate = lr)
     ss_model.test(n_test_eps)
     ss_model.plot_results()
     
     #Source-target
-    st_model = Model("CustomHopper-source-v0", "CustomHopper-target-v0")
-    st_model.train("SAC_s_0.003_50", n_timesteps, learning_rate = lr)
+    print("Source-target")
+    s_log_dir = "log_st_" + str(lr) + "_" + str(n_timesteps)
+    st_model = Model("CustomHopper-source-v0", "CustomHopper-target-v0", s_log_dir)
+    st_model.train("SAC_s_0.03_50000", n_timesteps, learning_rate = lr)
     st_model.test(n_test_eps)
-    st_model.plot_results()
+    #st_model.plot_results()
 
     #Target-target
-    tt_model = Model("CustomHopper-target-v0", "CustomHopper-target-v0")
-    #tt_model.train(n_timesteps, learning_rate = lr)
-    #tt_model.test(n_test_eps)
-    #tt_model.plot_results()
+    print("Target-target:")
+    s_log_dir = "log_tt_" + str(lr) + "_" + str(n_timesteps)
+    tt_model = Model("CustomHopper-target-v0", "CustomHopper-target-v0", s_log_dir)
+    tt_model.train(n_timesteps, learning_rate = lr)
+    tt_model.test(n_test_eps)
+    tt_model.plot_results()
 
     #Source-source using UDR
-    t_log_dir="log_t_"+str(lr)+"_"+str(n_timesteps)
-    ss_udr = Model("CustomHopper-source-v0", "CustomHopper-source-v0",t_log_dir)
+    print("Source-source with UDR:")
+    t_log_dir = "log_ss_" + str(lr) + "_" + str(n_timesteps)
+    ss_udr = Model("CustomHopper-source-v0", "CustomHopper-source-v0", t_log_dir)
     ss_udr.train_udr(None, n_timesteps, n_distr, learning_rate = lr)
     ss_udr.test(n_test_eps)
 
     #Source-target using UDR
+    print("Source-target with UDR:")
+    t_log_dir = "log_st_" + str(lr) + "_" + str(n_timesteps)
     st_udr = Model("CustomHopper-source-v0", "CustomHopper-target-v0")
-    #st_udr.train_udr(n_timesteps, n_distr, learning_rate = lr)
+    st_udr.train_udr(n_timesteps, n_distr, learning_rate = lr)
     st_udr.train_udr("SAC_s_0.003_50_UDR", n_timesteps, n_distr, learning_rate = lr)
     st_udr.test(n_test_eps)
 
